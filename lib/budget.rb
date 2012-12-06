e98f3e231f2c8c099e99eacfc3a689ce804bfd69
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-06 02:31:49 -0500

    Add updates and fixes to budget.rb

--- lib/budget.rb
+++ lib/budget.rb
@@ -4,6 +4,7 @@
 
 #requirements
 require 'date'
+require 'yaml'
 #STDOUT.flush #uncomment this if things screw up
 
 #classes
@@ -24,49 +25,40 @@ class Budget
 	end
 end
 
-def round(number,cash=true)
-	if cash
-		return "$#{((number*10**2).round.to_f/10**2).to_s}"
-	else
-		return ((number*10**2).round.to_f/10**2).to_s
-	end
+def round(number)
+	(number*10**2).round.to_f/10**2
 end
 
 #main function
 # What is your current RIT meal plan? Type 10, 12, 14, or ultra. If you want to track a budget for something else, type other.
 # Money left in budget
-def calculate_budget budget, money_left
-	#budget
-	case budget
-	when 10
-		budget = $m10plus
-	when 12
-		budget = $m12plus
-	when 14
-		budget = $m14plus
-	end
+def calculate_budget plan, money_left
+	data = YAML.load_file '../data/data.yml'
+	quarter = data['quarters']['2012-2']
+	money_total = data['plans'][2012][plan]
+
 	#inputs
-	date_start = budget.date_start
-	date_end = budget.date_end
-	money_total = budget.money_total
+	date_start = Date.strptime quarter['start'], '%m/%d/%Y'
+	date_end = Date.strptime quarter['end'], '%m/%d/%Y'
+
 	#calculations
 	days_left = date_end - Date.today
 	money_daily = money_left/days_left
 	{
 		:day_passed => (Date.today-date_start).to_i.to_s,
 		:day_total => (date_end-date_start).to_i.to_s,
-		:day_percent => round((((Date.today-date_start).to_f)/((date_end-date_start).to_f))*100,false),
+		:day_percent => round((((Date.today-date_start).to_f)/((date_end-date_start).to_f))*100),
 		:spent_amount => round(money_total-money_left),
 		:spent_total => round(money_total),
-		:spent_percent => round(((money_total-money_left)/money_total)*100,false),
+		:spent_percent => round(((money_total-money_left)/money_total)*100),
 		:spent_daily => round((money_total-money_left)/(Date.today-date_start)),
 		:recommended_daily_spending => round(money_daily),
 		:recommended_weekly_spending => round(money_daily*7)
 	}
 end
 
-def display_budget
-	results = calculate_budget
+def display_budget plan, money_left
+	results = calculate_budget plan, money_left
 	puts 'USAGE'
 	puts "Time: day #{results[:day_passed]} of #{results[:day_total]} (#{results[:day_passed_percent]}%)"
 	puts "Spent: #{results[:spent_amount]} of #{results[:spent_total]} (#{results[:spent_percent]}%), or #{results[:spent_daily]} daily"
@@ -75,4 +67,4 @@ def display_budget
 	puts "Spend #{results[:recommended_daily_spending]} daily (#{results[:recommended_weekly_spending]} weekly)."
 end
 
-display_budget
+display_budget '14plus', 200
