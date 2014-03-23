1729c1f2e66424d2a14d4974e6f4de1c801d5acc
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-23 03:58:39 -0400

    Use camel case for variable names

--- routes/index.js
+++ routes/index.js
@@ -20,48 +20,48 @@ function daysBetween(a, b) {
 // main function
 // What is your current RIT meal plan? Type 10, 12, 14, or ultra. If you want to track a budget for something else, type other.
 // Money left in budget
-function calculate_budget(plan, money_left) {
+function calculateBudget(plan, moneyLeft) {
   if (typeof plan === "String") {
     var quarter = data.quarters["2012-2"];
-    var money_total = data.plans["2012"][plan];
+    var moneyTotal = data.plans["2012"][plan];
 
     // inputs
-    var date_start = new Date(quarter.start);
-    var date_end = new Date(quarter.end);
+    var dateStart = new Date(quarter.start);
+    var dateEnd = new Date(quarter.end);
   } else {
-    var date_start = plan.date_start;
-    var date_end = plan.date_end;
-    var money_total = plan.money_total;
+    var dateStart = plan.dateStart;
+    var dateEnd = plan.dateEnd;
+    var moneyTotal = plan.moneyTotal;
   }
 
   // calculations
-  var days_left = daysBetween(new Date(), date_end);
-  var money_daily = money_left / days_left;
+  var daysLeft = daysBetween(new Date(), dateEnd);
+  var moneyDaily = moneyLeft / daysLeft;
   return {
-    day_passed: daysBetween(date_start, new Date()).toFixed(),
-    day_total: daysBetween(date_start, date_end).toFixed(),
-    day_percent: percent(daysBetween(date_start, new Date()), daysBetween(date_start, date_end)).toFixed(2),
-    spent_amount: (money_total - money_left).toFixed(2),
-    spent_total: parseInt(money_total, 10).toFixed(2),
-    spent_percent: percent(money_total - money_left, money_total).toFixed(2),
-    spent_daily: ((money_total - money_left) / daysBetween(date_start, new Date())).toFixed(2),
-    recommended_daily_spending: money_daily.toFixed(2),
-    recommended_weekly_spending: (money_daily * 7).toFixed(2)
+    dayPassed: daysBetween(dateStart, new Date()).toFixed(),
+    dayTotal: daysBetween(dateStart, dateEnd).toFixed(),
+    dayPercent: percent(daysBetween(dateStart, new Date()), daysBetween(dateStart, dateEnd)).toFixed(2),
+    spentAmount: (moneyTotal - moneyLeft).toFixed(2),
+    spentTotal: parseInt(moneyTotal, 10).toFixed(2),
+    spentPercent: percent(moneyTotal - moneyLeft, moneyTotal).toFixed(2),
+    spentDaily: ((moneyTotal - moneyLeft) / daysBetween(dateStart, new Date())).toFixed(2),
+    recommendedDailySpending: moneyDaily.toFixed(2),
+    recommendedWeeklySpending: (moneyDaily * 7).toFixed(2)
   };
 }
 
 exports.index = function(req, res){
-  if (req.query.date_start && req.query.date_end && req.query.money_total && req.query.money_left) {
+  if (req.query.dateStart && req.query.dateEnd && req.query.moneyTotal && req.query.moneyLeft) {
     var plan = {
-      date_start: new Date(req.query.date_start),
-      date_end: new Date(req.query.date_end),
-      money_total: req.query.money_total
+      dateStart: new Date(req.query.dateStart),
+      dateEnd: new Date(req.query.dateEnd),
+      moneyTotal: req.query.moneyTotal
     };
-    var money_left = req.query.money_left;
+    var moneyLeft = req.query.moneyLeft;
 
-    var results = calculate_budget(plan, money_left);
-  } else if (req.query.meal_plan && req.query.money_left) {
-    var results = calculate_budget(req.query.meal_plan, req.query.money_left);
+    var results = calculateBudget(plan, moneyLeft);
+  } else if (req.query.mealPlan && req.query.moneyLeft) {
+    var results = calculateBudget(req.query.mealPlan, req.query.moneyLeft);
   }
 
   res.render('index', {
